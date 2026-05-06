33d4fb7148c6fd6d49c42cdc97f09517ce383e73
thedjaswal-dotcom <thedjaswal@gmail.com>
2026-05-06 13:52:38 +0530

    Add: duplicate-integer - submission-0

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.py
@@ -0,0 +1,14 @@
+class Solution:
+    def hasDuplicate(self, nums) -> bool:
+        for i in nums:
+            if nums.count(i)>1:
+                return True
+            
+        return False
+    
+
+            
+                
+
+
+        
